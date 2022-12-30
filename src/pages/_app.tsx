1a0a66168f88279bd326583eda49533fa786a3e2
gabxyz <gabxyzdev@gmail.com>
2022-12-30 14:40:52 -0300

    Add @next/font and next-themes config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-
+import { ThemeProvider } from "next-themes";
+import { Inter } from "@next/font/google";
 import "../styles/globals.css";
 
+const interVariable = Inter();
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class" disableTransitionOnChange>
+      <main className={interVariable.className}>
+        <Component {...pageProps} />
+      </main>
+    </ThemeProvider>
+  );
 };
 
 export default MyApp;
