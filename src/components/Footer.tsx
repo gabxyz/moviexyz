90657f1930c8bcd8db97470e21f779b1b0af1e82
gabxyz <gabxyzdev@gmail.com>
2023-01-11 07:53:00 -0300

    Add Footer component and refactor layout to make it sticky

---
+++ src/components/Footer.tsx
@@ -0,0 +1,34 @@
+import { GitHubLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="mt-4 flex items-center justify-center gap-2 text-sm text-slate-11">
+        <p>
+          made by <span className="font-medium text-slate-12">gabxyz</span>
+        </p>
+        <div className="mt-0.5 inline-flex h-1 w-1 flex-none rounded-full bg-slate-11" />
+        <div className="flex items-center gap-1.5">
+          <a
+            className="cursor-pointer hover:text-slate-12 motion-safe:duration-300 motion-safe:ease-expressive-standard"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/gabxyz"
+          >
+            <GitHubLogoIcon />
+          </a>
+          <a
+            className="cursor-pointer hover:text-slate-12 motion-safe:duration-300 motion-safe:ease-expressive-standard"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/gabxyzdev"
+          >
+            <TwitterLogoIcon />
+          </a>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
