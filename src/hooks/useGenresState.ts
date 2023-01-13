531bc91629616ee40531b7cd8e6b3174f81b15dc
gabxyz <gabxyzdev@gmail.com>
2023-01-13 07:50:36 -0300

    Refactor useGenresState hook to persist state in localStorage

--- src/hooks/useGenresState.ts
+++ src/hooks/useGenresState.ts
@@ -1,13 +1,22 @@
 import create from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface GenresListState {
   genreIdList: string[];
   setGenreIdList: (selectedGenres: string[]) => void;
 }
 
-const useGenresState = create<GenresListState>((set) => ({
-  genreIdList: [],
-  setGenreIdList: (genreIdList: string[]) => set({ genreIdList }),
-}));
+const useGenresState = create<GenresListState>()(
+  persist(
+    (set) => ({
+      genreIdList: [],
+      setGenreIdList: (genreIdList: string[]) => set({ genreIdList }),
+    }),
+    {
+      name: "genreIdList",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
 
 export default useGenresState;
