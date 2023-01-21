96dd028b05361c99b334ca26f849a0fda3034d5e
gabxyz <gabxyzdev@gmail.com>
2023-01-20 21:23:57 -0300

    Refactor to use Link component in page title and add gradient styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,17 @@
 import SettingsModal from "@/components/SettingsModal";
 import AboutModal from "@/components/AboutModal";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="flex items-start justify-between rounded-xl border border-slate-6 bg-slate-2 p-4 shadow-md">
       <div className="flex flex-col">
-        <h1 className="text-lg font-bold leading-snug">Movie Explorer</h1>
+        <Link
+          href="/"
+          className="w-fit bg-gradient-to-r from-purple-11 to-indigo-11  bg-clip-text text-lg font-bold leading-snug text-transparent hover:opacity-80 motion-safe:duration-300 motion-safe:ease-expressive-standard"
+        >
+          Movie Explorer
+        </Link>
         <p className="text-sm text-slate-11">
           Explore and discover random movies
         </p>
