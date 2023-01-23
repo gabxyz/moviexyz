c46b65257fb4d69ecf86c4c0e5466cc73776a71a
gabxyz <gabxyzdev@gmail.com>
2023-01-23 14:54:30 -0300

    Refactor dynamic movie page to use getServerSideProps to fix Seo(hopefully)

--- src/pages/movie/[id].tsx
+++ src/pages/movie/[id].tsx
@@ -1,52 +1,51 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
+import type { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { AnimatePresence, motion } from "framer-motion";
-import MovieCard from "@/components/MovieCard";
-import useMovieData from "@/hooks/useMovieData";
+import { getMovieDetails } from "@/utils/tmdb";
 import useLetterCaseState from "@/hooks/useLetterCaseState";
 import Seo from "@/components/Seo";
+import MovieCard from "@/components/MovieCard";
 
-const MovieInfo = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  const parsedId = Number(id);
+const MoviePage = ({
+  movieData,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const { letterCase } = useLetterCaseState();
-  const { isLoading, movieTrigger, movieData } = useMovieData();
-
-  useEffect(() => {
-    if (parsedId) {
-      movieTrigger(parsedId);
-    }
-  }, [parsedId, movieTrigger]);
 
   return (
     <>
+      <Seo
+        title="moviexyz - random movies"
+        description="explore and discover random movies"
+        ogContent={`movieTitle=${movieData.title}&movieOverview=${movieData.overview}&moviePoster=${movieData.poster_path}&letterCase=${letterCase}`}
+      />
       <AnimatePresence mode="wait">
-        {!isLoading && movieData && (
-          <>
-            <Seo
-              title="moviexyz - random movies"
-              description="explore and discover random movies"
-              ogContent={`movieTitle=${movieData.title}&movieOverview=${movieData.overview}&moviePoster=${movieData.poster_path}&letterCase=${letterCase}`}
-            />
-            <motion.div
-              key={movieData.id}
-              initial={{ opacity: 0, y: -40 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0 }}
-              transition={{
-                type: "spring",
-                damping: 25,
-                stiffness: 250,
-              }}
-            >
-              <MovieCard {...movieData} />
-            </motion.div>
-          </>
-        )}
+        <motion.div
+          key={movieData.id}
+          initial={{ opacity: 0, y: -40 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0 }}
+          transition={{
+            type: "spring",
+            damping: 25,
+            stiffness: 250,
+          }}
+        >
+          <MovieCard {...movieData} />
+        </motion.div>
       </AnimatePresence>
     </>
   );
 };
 
-export default MovieInfo;
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { id } = context.query;
+  const parsedId = Number(id);
+  const movieData = await getMovieDetails(parsedId);
+
+  return {
+    props: {
+      movieData,
+    },
+  };
+};
+
+export default MoviePage;
