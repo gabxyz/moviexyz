24b2d887f92538e846114b0a0013fb446065879c
gabxyz <gabxyzdev@gmail.com>
2023-01-26 15:22:15 -0300

    hehehhehehehe

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -22,7 +22,7 @@ export default async function handler(req: NextRequest) {
       interBold,
     ]);
 
-    const { searchParams } = new URL(req.url);
+    const { searchParams } = req.nextUrl;
     const poster = searchParams.get("moviePoster");
     const title = searchParams.get("movieTitle");
     const overview = searchParams.get("movieOverview");
@@ -91,24 +91,40 @@ export default async function handler(req: NextRequest) {
           ],
         }
       );
-    }
-    return new ImageResponse(
-      (
-        <div
-          tw={
-            "flex w-full h-full justify-between items-center text-lg p-10 lowercase"
-          }
-          style={{
-            backgroundImage: "linear-gradient(120deg, #3a1e48, #1c274f)",
-            color: "#9ba1a6",
-            fontFamily: "Inter",
-          }}
-        >
-          <div tw="w-full h-full flex justify-between items-center rounded-xl shadow-lg px-20 bg-[#202425] border border-[#3a3f42]">
-            <div tw="flex flex-col max-w-md justify-center h-[534px]">
-              <div tw="flex flex-col items-center">
-                <h2
-                  tw="text-4xl"
+    } else {
+      return new ImageResponse(
+        (
+          <div
+            tw={
+              "flex w-full h-full justify-between items-center text-lg p-10 lowercase"
+            }
+            style={{
+              backgroundImage: "linear-gradient(120deg, #3a1e48, #1c274f)",
+              color: "#9ba1a6",
+              fontFamily: "Inter",
+            }}
+          >
+            <div tw="w-full h-full flex justify-between items-center rounded-xl shadow-lg px-20 bg-[#202425] border border-[#3a3f42]">
+              <div tw="flex flex-col max-w-md justify-center h-[534px]">
+                <div tw="flex flex-col items-center">
+                  <h2
+                    tw="text-4xl"
+                    style={{
+                      backgroundImage:
+                        "linear-gradient(90deg, #bf7af0, #849dff)",
+                      backgroundClip: "text",
+                      WebkitBackgroundClip: "text",
+                      color: "transparent",
+                    }}
+                  >
+                    moviexyz
+                  </h2>
+                  <p tw="-mt-2 text-xl">explore and discover random movies</p>
+                </div>
+              </div>
+              <div tw="flex items-center justify-center w-[350px] h-[500px] bg-[#1a1d1e] rounded-lg border-2 border-[#313538]">
+                <div
+                  tw="text-[128px] font-bold"
                   style={{
                     backgroundImage: "linear-gradient(90deg, #bf7af0, #849dff)",
                     backgroundClip: "text",
@@ -116,46 +132,32 @@ export default async function handler(req: NextRequest) {
                     color: "transparent",
                   }}
                 >
-                  moviexyz
-                </h2>
-                <p tw="-mt-2 text-xl">explore and discover random movies</p>
-              </div>
-            </div>
-            <div tw="flex items-center justify-center w-[350px] h-[500px] bg-[#1a1d1e] rounded-lg border-2 border-[#313538]">
-              <div
-                tw="text-[128px] font-bold"
-                style={{
-                  backgroundImage: "linear-gradient(90deg, #bf7af0, #849dff)",
-                  backgroundClip: "text",
-                  WebkitBackgroundClip: "text",
-                  color: "transparent",
-                }}
-              >
-                ?
+                  ?
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ),
-      {
-        width: 1200,
-        height: 630,
-        fonts: [
-          {
-            name: "Inter",
-            data: interMediumFont,
-            style: "normal",
-            weight: 500,
-          },
-          {
-            name: "Inter",
-            data: interBoldFont,
-            style: "normal",
-            weight: 700,
-          },
-        ],
-      }
-    );
+        ),
+        {
+          width: 1200,
+          height: 630,
+          fonts: [
+            {
+              name: "Inter",
+              data: interMediumFont,
+              style: "normal",
+              weight: 500,
+            },
+            {
+              name: "Inter",
+              data: interBoldFont,
+              style: "normal",
+              weight: 700,
+            },
+          ],
+        }
+      );
+    }
   } catch (e: any) {
     console.log(`${e.message}`);
     return new Response(`Failed to generate the image`, {
