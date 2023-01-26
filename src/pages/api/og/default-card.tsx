8636d2060593fe086d39d4065fc70ed20d6f4471
gabxyz <gabxyzdev@gmail.com>
2023-01-26 18:44:15 -0300

    Fix imports

--- src/pages/api/og/default-card.tsx
+++ src/pages/api/og/default-card.tsx
@@ -7,11 +7,11 @@ export const config = {
 };
 
 const interMedium = fetch(
-  new URL("../../../public/fonts/Inter-Medium.ttf", import.meta.url)
+  new URL("../../../../public/fonts/Inter-Medium.ttf", import.meta.url)
 ).then((res) => res.arrayBuffer());
 
 const interBold = fetch(
-  new URL("../../../public/fonts/Inter-Bold.ttf", import.meta.url)
+  new URL("../../../../public/fonts/Inter-Bold.ttf", import.meta.url)
 ).then((res) => res.arrayBuffer());
 
 export default async function handler() {
