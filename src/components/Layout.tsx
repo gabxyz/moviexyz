be55c5725329bd93f0cbd751af29f10829e50b4c
gabxyz <gabxyzdev@gmail.com>
2023-01-27 10:12:51 -0300

    Refactor layout to test new data fetching strategy

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,32 +1,45 @@
 import { useRouter } from "next/router";
-import useSWRMutation from "swr/mutation";
+// import useSWR from "swr";
+import useSWRImmutable from "swr/immutable";
 import { AnimatePresence, m } from "framer-motion";
 import { Loader2 } from "lucide-react";
 import useGenresState from "@/hooks/useGenresState";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { useCallback } from "react";
 
 type LayoutProps = {
   children?: React.ReactNode;
 };
 
-const randomIdFetcher = (url: string, { arg }: { arg?: string }) =>
-  fetch(arg ? `${url}?genresId=${arg}` : url).then((res) => res.json());
+// const randomIdFetcher = (url: string, { arg }: { arg?: string }) =>
+//   fetch(arg ? `${url}?genresId=${arg}` : url).then((res) => res.json());
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Layout = ({ children }: LayoutProps) => {
   const router = useRouter();
   const { genreIdList } = useGenresState();
   const genresParsed = genreIdList.join("|");
 
-  const { trigger: getRandomId, isMutating: isLoading } = useSWRMutation(
-    "/api/randomId",
-    randomIdFetcher
+  // const { trigger: getRandomId, isMutating: isLoading } = useSWRMutation(
+  //   "/api/randomId",
+  //   randomIdFetcher
+  // );
+  const { data, mutate, isValidating } = useSWRImmutable(
+    `/api/randomId?genresId=${genresParsed}`,
+    fetcher
   );
 
-  const handleClick = async () => {
-    const randomIdRes = await getRandomId(genresParsed);
-    router.push(`/movie/${randomIdRes.id}`);
-  };
+  // const handleClick = async () => {
+  //   const randomIdRes = await getRandomId(genresParsed);
+  //   router.push(`/movie/${randomIdRes.id}`);
+  // };
+
+  const handleClick = useCallback(() => {
+    mutate(`/api/randomId?genresId=${genresParsed}`);
+    router.push(`/movie/${data?.id}`);
+  }, [genresParsed, data, mutate, router]);
 
   return (
     <>
@@ -35,10 +48,10 @@ const Layout = ({ children }: LayoutProps) => {
         <div className="self-center p-6">
           <button
             onClick={handleClick}
-            disabled={isLoading}
+            disabled={isValidating}
             className="h-8 rounded-lg border border-slate-7 bg-slate-3 px-3 text-sm font-medium shadow hover:border-slate-8 hover:bg-slate-4 motion-safe:duration-300 motion-safe:ease-expressive-standard"
           >
-            {isLoading ? (
+            {isValidating ? (
               <Loader2 size={16} className="animate-spin" />
             ) : (
               <p>Pick random movie</p>
