8556ee8014068b771e296c30f7517fe3fb23e97e
gabxyz <gabxyzdev@gmail.com>
2023-02-05 21:09:25 -0300

    refactor og image cards to use satoshi local font and better styling

--- src/pages/api/og/default-card.tsx
+++ src/pages/api/og/default-card.tsx
@@ -6,35 +6,35 @@ export const config = {
   runtime: "edge",
 };
 
-const interMedium = fetch(
-  new URL("../../../../public/fonts/Inter-Medium.ttf", import.meta.url)
+const satoshiMedium = fetch(
+  new URL("../../../../public/fonts/Satoshi-Medium.ttf", import.meta.url)
 ).then((res) => res.arrayBuffer());
 
-const interBold = fetch(
-  new URL("../../../../public/fonts/Inter-Bold.ttf", import.meta.url)
+const satoshiBold = fetch(
+  new URL("../../../../public/fonts/Satoshi-Bold.ttf", import.meta.url)
 ).then((res) => res.arrayBuffer());
 
 export default async function handler() {
   try {
-    const [interMediumFont, interBoldFont] = await Promise.all([
-      interMedium,
-      interBold,
+    const [satoshiMediumFont, satoshiBoldFont] = await Promise.all([
+      satoshiMedium,
+      satoshiBold,
     ]);
 
     return new ImageResponse(
       (
         <div
           tw={
-            "flex w-full h-full justify-between items-center text-lg p-10 lowercase"
+            "flex w-full h-full justify-between items-center text-lg px-10 py-12 lowercase"
           }
           style={{
             backgroundImage: "linear-gradient(120deg, #3a1e48, #1c274f)",
             color: "#9ba1a6",
-            fontFamily: "Inter",
+            fontFamily: "Satoshi",
           }}
         >
-          <div tw="w-full h-full flex justify-between items-center rounded-xl shadow-lg px-18 bg-[#202425] border-2 border-[#3a3f42]">
-            <div tw="flex flex-col justify-around items-center max-w-md mt-28 h-[534px]">
+          <div tw="w-full h-full flex justify-between items-center rounded-2xl shadow-lg px-4 bg-[#202425] border-2 border-[#3a3f42]">
+            <div tw="flex flex-col justify-between items-center w-1/2 h-[484px]">
               <div tw="flex flex-col items-center">
                 <h2
                   tw="text-4xl"
@@ -84,14 +84,14 @@ export default async function handler() {
         height: 630,
         fonts: [
           {
-            name: "Inter",
-            data: interMediumFont,
+            name: "Satoshi",
+            data: satoshiMediumFont,
             style: "normal",
             weight: 500,
           },
           {
-            name: "Inter",
-            data: interBoldFont,
+            name: "Satoshi",
+            data: satoshiBoldFont,
             style: "normal",
             weight: 700,
           },
--- src/pages/api/og/movie-card.tsx
+++ src/pages/api/og/movie-card.tsx
@@ -7,19 +7,19 @@ export const config = {
   runtime: "edge",
 };
 
-const interMedium = fetch(
-  new URL("../../../../public/fonts/Inter-Medium.ttf", import.meta.url)
+const satoshiMedium = fetch(
+  new URL("../../../../public/fonts/Satoshi-Medium.ttf", import.meta.url)
 ).then((res) => res.arrayBuffer());
 
-const interBold = fetch(
-  new URL("../../../../public/fonts/Inter-Bold.ttf", import.meta.url)
+const satoshiBold = fetch(
+  new URL("../../../../public/fonts/Satoshi-Bold.ttf", import.meta.url)
 ).then((res) => res.arrayBuffer());
 
 export default async function handler(req: NextRequest) {
   try {
-    const [interMediumFont, interBoldFont] = await Promise.all([
-      interMedium,
-      interBold,
+    const [satoshiMediumFont, satoshiBoldFont] = await Promise.all([
+      satoshiMedium,
+      satoshiBold,
     ]);
 
     const { searchParams } = req.nextUrl;
@@ -34,19 +34,19 @@ export default async function handler(req: NextRequest) {
       (
         <div
           tw={
-            "flex w-full h-full justify-between items-center text-lg p-10 lowercase"
+            "flex w-full h-full justify-between items-center text-lg px-10 py-12 lowercase"
           }
           style={{
             backgroundImage: "linear-gradient(120deg, #3a1e48, #1c274f)",
             color: "#9ba1a6",
-            fontFamily: "Inter",
+            fontFamily: "Satoshi",
           }}
         >
-          <div tw="w-full h-full flex justify-between items-center rounded-xl shadow-lg px-18 bg-[#202425] border-2 border-[#3a3f42]">
-            <div tw="flex flex-col max-w-md justify-between h-[534px]">
-              <div tw="flex flex-col">
+          <div tw="w-full h-full flex justify-between items-center rounded-2xl shadow-lg px-4 bg-[#202425] border-2 border-[#3a3f42]">
+            <div tw="flex flex-col justify-between items-center w-1/2 h-[484px]">
+              <div tw="flex flex-col items-center">
                 <h2
-                  tw="text-4xl mt-6"
+                  tw="text-4xl"
                   style={{
                     backgroundImage: "linear-gradient(90deg, #bf7af0, #849dff)",
                     backgroundClip: "text",
@@ -56,14 +56,14 @@ export default async function handler(req: NextRequest) {
                 >
                   moviexyz
                 </h2>
-                <p tw="-mt-4 text-xl">explore and discover random movies</p>
+                <p tw="-mt-2 text-xl">explore and discover random movies</p>
               </div>
-              <div tw="flex flex-col">
+              <div tw="flex flex-col items-center">
                 <h3 tw="text-xl text-[#ecedee]">{title}</h3>
-                <p tw="-mt-4 overflow-hidden">{parsedOverview}</p>
+                <p tw="-mt-2 overflow-hidden text-center">{parsedOverview}</p>
               </div>
               <div
-                tw="text-lg mb-6"
+                tw="text-lg"
                 style={{
                   backgroundImage: "linear-gradient(240deg, #bf7af0, #849dff)",
                   backgroundClip: "text",
@@ -74,7 +74,7 @@ export default async function handler(req: NextRequest) {
                 moviexyz.vercel.app
               </div>
             </div>
-            <div tw="flex items-center">
+            <div tw="flex items-center justify-center">
               <img
                 src={`https://image.tmdb.org/t/p/w500${poster}`}
                 alt={`poster image for the movie ${title}`}
@@ -89,14 +89,14 @@ export default async function handler(req: NextRequest) {
         height: 630,
         fonts: [
           {
-            name: "Inter",
-            data: interMediumFont,
+            name: "Satoshi",
+            data: satoshiMediumFont,
             style: "normal",
             weight: 500,
           },
           {
-            name: "Inter",
-            data: interBoldFont,
+            name: "Satoshi",
+            data: satoshiBoldFont,
             style: "normal",
             weight: 700,
           },
